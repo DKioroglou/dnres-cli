1397ba561679f61f864c0e4b34d6071e4d76bd39
Dimitrios Kioroglou <dkioroglou@cicbiogune.es>
2023-02-27 12:41:31 +0100

    Version with tagging system

--- src/dnres_cli.py
+++ src/dnres_cli.py
@@ -1,15 +1,13 @@
 import click
-import contextlib
-from datetime import datetime
 from dnres import DnRes
-import configparser
 import os
 import pandas as pd
-from pyfzf.pyfzf import FzfPrompt
-import subprocess
-import sqlite3
 import json
-from flask import Flask, render_template, send_from_directory
+
+
+def _check_path_in_structure(res, path):
+    if not os.path.exists(os.path.join(res.structure, path)):
+        exit("Path not in structure")
 
 
 @click.group(invoke_without_command=True)
@@ -18,7 +16,7 @@ from flask import Flask, render_template, send_from_directory
 def dnres(ctx, config):
     """
     \b
-    Prints the contents of the structure is no command is passed.
+    Prints the contents of the structure if no command is passed.
     """
 
     res = DnRes(config)
@@ -29,202 +27,102 @@ def dnres(ctx, config):
 
 
 @dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory.')
-@click.option('--filename', '-f', required=False, help='Filename in directory.')
+@click.option('--path', '-p', help='Path to show information about.')
 @click.pass_obj
-def info(res, directory, filename):
+def info(res, path):
     """
     \b
-    Shows information for directory or filename.
-    Otherwise, it shows information for the passed filename of the passed directory.
+    Shows information for given path.
     """
 
-    if not filename:
-        res.info_directory(directory)
-    else:
-        res.info_filename(directory, filename)
+    res.info(path)
 
 
 @dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory.')
-@click.option('--filename', '-f', required=True, help='Filename in directory.')
+@click.option('--tag', '-t', help='Tag for path')
+@click.option('--path', '-p', help='Path to be tagged.')
 @click.pass_obj
-def delete(res, directory, filename):
+def tag(res, tag, path):
     """
     \b
-    Deletes stored object. 
+    Add tag to given path.
     """
 
-    res.delete(directory, filename)
+    _check_path_in_structure(res, path)
+    res.tag(tag, path)
 
 
 @dnres.command()
-@click.option('--filename', '-f', required=True, help='Filename to move.')
-@click.option('--source', '-s', required=True, help='Directory in structure where data are stored.')
-@click.option('--destination', '-d', required=True, help='Directory in structure where data will be moved to.')
-@click.option('--overwrite', is_flag=True, help='Flag for overwriting previously stored data under same filename.')
-@click.option('--db_only', is_flag=True, help='Flag for making changes only in database in case filename is reference to path.')
+@click.option('--path', '-p', help='Path to set info.')
+@click.option('--datatype', '-d', required=False, help='Datatype of path.')
+@click.option('--description', '-i', required=False, help='Short description about the data.')
+@click.option('--source', '-s', required=False, help='Source that generated the data.')
 @click.pass_obj
-def move(res, filename, source, destination, overwrite, db_only):
+def set_info(res, path, datatype, description, source):
     """
     \b
-    Moves stored objects from source to destination.
-    If overwrite, stored objects with same name will be overwriten in destination.
+    For existing paths, it sets information for given path.
+    For new paths, it registers it to the database and sets the provided information. 
     """
 
-    if db_only:
-        res.move(filename=filename,
-                 source=source,
-                 destination=destination,
-                 overwrite=overwrite,
-                 db_only=True)
-    else:
-        res.move(filename=filename,
-                 source=source,
-                 destination=destination,
-                 overwrite=overwrite,
-                 db_only=False)
+    _check_path_in_structure(res, path)
+    res.set_info(path, datatype, description, source)
 
 
 @dnres.command()
-@click.option('--directory', '-d', required=True, help='Directory where stored object is.')
-@click.option('--previous', '-p', required=True, help='Previous filename of stored object.')
-@click.option('--new', '-n', required=True, help='New filename of stored object.')
+@click.option('--path', '-p', help='Path to be removed.')
 @click.pass_obj
-def rename(res, directory, previous, new):
+def remove_from_db(res, path):
     """
     \b
-    Renames stored object from previous filename to new filename.
+    Removes path from database.
     """
 
-    res.rename(directory=directory,
-               previous=previous,
-               new=new)
+    res.remove_from_db(path)
 
 
 @dnres.command()
-@click.option('--description', '-i', required=True, help='Description to set for stored object.')
-@click.option('--directory', '-d', required=True, help='Directory where stored object is.')
-@click.option('--filename', '-f', required=True, help='Filename of the stored object.')
+@click.option('--tag', '-t', help='Tag to be removed.')
+@click.option('--path', '-p', help='Path to remove tag from.')
 @click.pass_obj
-def set_description(res, description, directory, filename):
+def remove_tag(res, tag, path):
     """
     \b
-    Sets description for stored object.
+    Removes tag from given path.
     """
 
-    res.set_description(description=description,
-                        directory=directory,
-                        filename=filename)
+    res.remove_tag(tag, path)
 
 
 @dnres.command()
-@click.option('--datatype', '-i', required=True, help='Datatype to set for stored object.')
-@click.option('--directory', '-d', required=True, help='Directory where stored object is.')
-@click.option('--filename', '-f', required=True, help='Filename of the stored object.')
+@click.option('--path', '-p', help='Path to show absolute path.')
 @click.pass_obj
-def set_datatype(res, datatype, directory, filename):
+def ls(res, path):
     """
     \b
-    Sets datatype for stored object.
+    Prints the absolute path of the provided path.
     """
 
-    res.set_datatype(datatype=datatype,
-                    directory=directory,
-                    filename=filename)
-
-
-@dnres.command()
-@click.option('--source', '-s', required=True, help='Source information to set for stored object.')
-@click.option('--directory', '-d', required=True, help='Directory where stored object is.')
-@click.option('--filename', '-f', required=True, help='Filename of the stored object.')
-@click.pass_obj
-def set_source(res, source, directory, filename):
-    """
-    \b
-    Sets source information for stored object.
-    """
-
-    res.set_source(source=source,
-                   directory=directory,
-                   filename=filename)
-
-
-@dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory.')
-@click.option('--filename', '-f', required=True, help='Filename in directory.')
-@click.pass_obj
-def ls(res, directory, filename):
-    """
-    \b
-    Prints the filepath of the stored object or file.
-    """
-
-    filepath = os.path.join(res.structure[directory], filename)
+    filepath = os.path.join(res.structure, path)
     print(filepath)
 
 
-@dnres.command()
-@click.option('--data', required=True, help='Name of data to store.')
-@click.option('--directory', '-d', required=True, help='Name of directory to store to.')
-@click.option('--filename', '-f', required=True, help='Filename under which data will be stored.')
-@click.option('--description', '-i', required=False, help='Brief description about the data.')
-@click.option('--source', '-s', required=False, help='Where data came from.')
-@click.option('--register', is_flag=True, help='Flag in case you only want to register file path to database.')
-@click.option('--overwrite', is_flag=True, help='Flag for overwriting previously stored data under same filename.')
-@click.pass_obj
-def store(res, data, directory, filename, description, source, register, overwrite):
-    """
-    \b
-    Stores data in structure and database.
-    """
-    
-    res.store(data=data,
-              directory=directory,
-              filename=filename,
-              source=source,
-              description=description,
-              register=register,
-              isfile=True,
-              overwrite=overwrite)
-
 
 @dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory in db to store to.')
-@click.option('--filename', '-f', required=True, help='How the registered directory will be referenced in the db.')
-@click.option('--description', '-i', required=False, help='Brief description about the directory.')
-@click.option('--source', '-s', required=False, help='Where directory came from.')
-@click.pass_obj
-def insert_dir_to_db(res, directory, filename, description, source):
-    """
-    \b
-    Register whole directory that holds data to database.
-    """
-
-    date = int(datetime.today().strftime('%Y%m%d'))
-    datatype = 'dir'
-    res._insert_in_db(directory, 
-                       date,
-                       filename,
-                       datatype,
-                       description,
-                       source)
-
-
-@dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory.')
-@click.option('--filename', '-f', required=True, help='Filename in directory.')
+@click.option('--path', '-p', help='Path to show data from.')
 @click.option('--backend', '-b', required=True, type=click.Choice(['pandas', 'none']), 
               default='none', show_default=True, help="Backend to use in order to load and print objects or files.")
 @click.option('--delimiter', required=False, type=click.Choice(['tab', 'comma']), help="Delimiter for csv or tsv files.")
 @click.option('--sheet', type=int, required=False, help="Sheet number for excel files.")
 @click.pass_obj
-def cat(res, directory, filename, backend, delimiter, sheet):
+def cat(res, path, backend, delimiter, sheet):
     """
     \b
-    It prints the contents of the stored object or file. 
+    It prints the contents of the stored object or path. 
     Prints filepath if stored data are not supported for printing.
     """
+
+    _check_path_in_structure(path)
 
     # Identify object is serialized
     if filename.endswith(".json") or filename.endswith(".pickle"):
@@ -233,7 +131,7 @@ def cat(res, directory, filename, backend, delimiter, sheet):
         serialization = False
    
     if serialization:
-        data = res.load(directory, filename)
+        data = res.load(path)
 
         if isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):
             for item in data:
@@ -249,13 +147,13 @@ def cat(res, directory, filename, backend, delimiter, sheet):
             print(data.to_csv(index=False, sep='\t'))
 
         else:
-            print(os.path.join(res.structure[directory], filename))
+            print(os.path.join(res.structure, path))
 
     else:
-        filepath = res.load(directory, filename)
+        filepath = res.load(path)
 
         # Action for TXT files
-        if filename.endswith('.txt'):
+        if filepath.endswith('.txt'):
             if backend and backend != 'none':
                 raise Exception('For txt file backend should be none.')
             with open(filepath, 'r') as inf:
@@ -264,7 +162,7 @@ def cat(res, directory, filename, backend, delimiter, sheet):
                     print(line)
 
         # Action for CSV or TSV files
-        elif filename.endswith('.csv') or filename.endswith('.tsv'):
+        elif filepath.endswith('.csv') or filepath.endswith('.tsv'):
             if backend == 'none':
                 with open(filepath, 'r') as inf:
                     for line in inf:
@@ -282,7 +180,7 @@ def cat(res, directory, filename, backend, delimiter, sheet):
                 print(df.to_string())
 
         # Action for EXCEL files
-        elif filename.endswith('.xls') or filename.endswith('.xlsx'):
+        elif filepath.endswith('.xls') or filepath.endswith('.xlsx'):
             if backend == 'none':
                 raise Exception("For excel files, backend cannot be none.")
             elif backend == 'pandas':
@@ -293,71 +191,6 @@ def cat(res, directory, filename, backend, delimiter, sheet):
 
         else:
             print(filepath)
-
-
-
-@dnres.command()
-@click.option('--directory', '-d', required=True, help='Name of directory.')
-@click.option('--filename', '-f', required=True, help='Filename in directory.')
-@click.pass_obj
-def view(res, directory, filename):
-    """
-    \b
-    Use a user defined viewer to view the file.
-    """
-
-    # Identify object is serialized
-    if filename.endswith(".json") or filename.endswith(".pickle"):
-        serialization = True
-    else:
-        serialization = False
-   
-    if serialization:
-        raise Exception("File is serialized. Use 'cat' command.")
-    else:
-        filepath = res.load(directory, filename)
-
-        config = configparser.ConfigParser()
-        config.read(res.config_file)
-        if not config.has_section('VIEWER'):
-            raise KeyError("With 'view' command, VIEWER section is required in the config file.")
-        viewers = config['VIEWER'].keys()
-        fzf = FzfPrompt()
-        try:
-            choice = fzf.prompt(viewers, '--exact')
-        except Exception:
-            exit("Action cancelled.")
-        choice = choice[0]
-        command = config['VIEWER'][choice]
-        command = command.format(filepath)
-        subprocess.run(command, shell=True)
-
-
-@dnres.command()
-@click.pass_obj
-def webapp(res):
-    """
-    \b
-    Starts a Flask server to view data and results."""
-    app = Flask(__name__)
-
-    @app.route('/')
-    def index():
-        data = dict()
-        with contextlib.closing(sqlite3.connect(res.db)) as conn:
-            with contextlib.closing(conn.cursor()) as c:
-                for directory in res.structure.keys():
-                    query = f"SELECT * FROM {directory}"
-                    c.execute(query)
-                    results = c.fetchall()
-                    data[directory] = results
-        return render_template('index.html', description=res.description, data=data)
-
-    @app.route('/<directory>/<path:filename>')
-    def serve_file(directory, filename):
-        return send_from_directory(res.structure[directory], filename)
-
-    app.run(debug=True, port=7000)
 
 
 if __name__ == "__main__":
